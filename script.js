3d19b496c89b51df4c35fb906edbea3c7d53e980
Luis <luisitex2085@gmail.com>
2024-11-27 17:14:56 +0100

    Corrección de estilos y funcionalidad
      -Se corrigió orden y y colores de los divs para mejor visualización.
      -Se implementó funcionalidad 'click' sobre los títulos para mostrar
       eventos.

--- script.js
+++ script.js
@@ -82,27 +82,71 @@ function nodoEventos(dia, data) {
     for (let i = 0; i < data.length; i++) {
         let fechaInicio = new Date(data[i].fechaAlta);
         let fechaFin = new Date(data[i].fechaFinalizacion);
-        fechaInicio.setHours(0,0,0,0);
-        fechaFin.setHours(0,0,0,0);
-     
         let divcolor = document.createElement("div");
         divcolor.classList.add("divcolor");
         divcolor.style.backgroundColor = data[i].color;
-
-        //comparamos fecha-calendario con fecha-evento,  solo fechas(sin horas)
+      
+        //comparamos fecha-calendario con fecha-evento,  solo fechas(sin horas) muestra detalles (primer dia evento)
         if (fecha.toDateString() == new Date(fechaInicio).toDateString()) {
+            let divtitEven = document.createElement("div");
+            divtitEven.classList.add("divtitEven");
+
             let h2 = document.createElement("h2");
             h2.classList.add("titulo");
             h2.textContent = data[i].titulo;
             h2.style.backgroundColor = data[i].color;
-            divTitulos.appendChild(h2);  
+            divtitEven.appendChild(h2)
+            divTitulos.appendChild(divtitEven);
+            //creamos div para restode eventos
+            let totalEventos = document.createElement("div");
+            totalEventos.classList.add("totalEventos");
+
+            //resto de eventos
+            let inicio = document.createElement("p");
+            inicio.textContent = `Inicio: ${data[i].fechaAlta}`;
+            totalEventos.appendChild(inicio);
+            divtitEven.appendChild(totalEventos);
+           
+            let fin=document.createElement("p");
+            fin.style.backgroundColor = data[i].color;
+            fin.textContent = `Fin: ${data[i].fechaFinalizacion}`;
+            totalEventos.appendChild(fin);
+
+            let cliente=document.createElement("p");
+            cliente.textContent = `Cliente: ${data[i].cliente}`;
+            totalEventos.appendChild(cliente);
+
+            let contacto=document.createElement("p");
+            contacto.textContent = `contacto: ${data[i].contacto}`;
+            totalEventos.appendChild(contacto);
+
+            divTitulos.appendChild(divtitEven);
+
+                //aplicamos evento click en h2
+             h2.addEventListener("click", function () {
+                 if (!totalEventos.style.display || totalEventos.style.display === "none") {
+                     totalEventos.style.display = "block"; 
+                     
+                 } else {
+                     totalEventos.style.display = "none"; 
+                 }
+             });
+            
+            // document.addEventListener("click", function (e) {
+            //     if(!e.target.matches(".titulo")){
+            //         totalEventos.style.display="none";
+            //     }else{
+            //         totalEventos.style.display="block";
+            //     }
+            // });
             nodos.push(divTitulos);
+            
         }
         //fechaInicio y fechaFin
         else if (fecha.getTime() > new Date(fechaInicio).getTime() && fecha.getTime() <= new Date(fechaFin).getTime()) {
             let divColor = document.createElement("div");
             divColor.classList.add("divColor");
-            divColor.style.backgroundColor=data[i].color;
+            divColor.style.backgroundColor = data[i].color;
             divTitulos.appendChild(divColor);
             nodos.push(divTitulos);
         }
